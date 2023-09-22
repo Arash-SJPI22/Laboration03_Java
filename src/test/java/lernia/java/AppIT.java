696a274d67b616c9ec13a8cd4d5d15d3783c2b32
Arash-SJPI22 <arash.eblaghian@lerniastudent.se>
2023-09-22 16:10:12 +0200

    Structure from maven-java-template

---
+++ src/test/java/lernia/java/AppIT.java
@@ -0,0 +1,12 @@
+package lernia.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppIT {
+    @Test
+    void itTest() {
+        assertThat(false).isFalse();
+    }
+}
---
+++ src/test/java/lernia/java/AppTest.java
@@ -0,0 +1,12 @@
+package lernia.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AppTest {
+    @Test
+    void test() {
+        assertThat(true).isTrue();
+    }
+}
